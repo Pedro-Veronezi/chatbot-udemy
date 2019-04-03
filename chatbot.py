3b0f4bff10f5f8dcd713caffaf04c55450adba6e
Pedro Veronezi <pveronezi@localhost.localdomain>
2019-04-03 08:30:59 -0300

    added .gitignore

--- chatbot.py
+++ chatbot.py
@@ -6,3 +6,56 @@ Created on Thu Mar  7 20:33:38 2019
 @author: pveronezi
 """
 
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+lines = open('movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+id2line = {}
+
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4] 
+
+conversations_ids = []
+
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))    
+
+questions = []
+answers = []
+
+for ci in conversations_ids:
+    for i in range(len(ci)-1):
+        questions.append(id2line[ci[i]])
+        answers.append(id2line[ci[i+1]])
+        
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"[-()\"#/@<>;:{}+=~|.?,!]", "", text)
+    return text
+
+clean_question = []
+clean_answers = []
+for q in questions:
+    clean_question.append(clean_text(q))
+for a in answers:
+    clean_answers.append(clean_text(a))    
+    
